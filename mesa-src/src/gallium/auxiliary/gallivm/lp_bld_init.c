2ef3e66a0ad5338a990bb9c6aa7c322c7f61cb6c
Mike Hommey <mh@glandium.org>
2021-09-21 12:42:39 +0900

    Use InstSimplify pass instead of ConstantProgagation
    The constantPropagation pass was removed in LLVM 12.
    
    Backported from
    https://gitlab.freedesktop.org/mesa/mesa/-/commit/52cac068621a5998f486f8e44f9c2d9d045d1c31
    https://gitlab.freedesktop.org/mesa/mesa/-/commit/1d018bc7fde744b5fc71108887a51e5bfaff8776

--- mesa-src/src/gallium/auxiliary/gallivm/lp_bld_init.c
+++ mesa-src/src/gallium/auxiliary/gallivm/lp_bld_init.c
@@ -169,7 +169,11 @@ create_pass_manager(struct gallivm_state *gallivm)
        */
       LLVMAddReassociatePass(gallivm->passmgr);
       LLVMAddPromoteMemoryToRegisterPass(gallivm->passmgr);
+#if LLVM_VERSION_MAJOR <= 11
       LLVMAddConstantPropagationPass(gallivm->passmgr);
+#else
+      LLVMAddInstructionSimplifyPass(gallivm->passmgr);
+#endif
       LLVMAddInstructionCombiningPass(gallivm->passmgr);
       LLVMAddGVNPass(gallivm->passmgr);
    }
